4e3579ba0658f8212ded7f06de61e4706a7fabe1
FlanB <clement.renou@mmibordeaux.com>
2023-11-20 11:38:39 +0100

    feat(debug): add scene binding

--- src/WebGL/Components/SceneComponent.js
+++ src/WebGL/Components/SceneComponent.js
@@ -16,7 +16,7 @@ export default class SceneComponent {
 		this.resource = this.resources.items.sceneModel
 
 		this.options = {
-			scene: 1,
+			scene: 0,
 			sceneParams: [
 				{
 					cameraZPosition: 4,
@@ -43,24 +43,27 @@ export default class SceneComponent {
 	}
 
 	setSection(sectionNumber) {
+		this.options.scene = sectionNumber
 		gsap.to(this.paint.position, {
 			z: this.options.sceneParams[sectionNumber].paintZPosition,
 		})
-		gsap.to(this.experience.camera.instance.position, {
+		gsap.to(this.camera.instance.position, {
 			z: this.options.sceneParams[sectionNumber].cameraZPosition,
 			delay: 0.25,
 		})
-		gsap.to(this.experience.camera.options, {
-			keyframes: [
-				{ ease: 'power1.in', duration: 0.75, fov: 70 },
-				{ ease: 'power1.out', duration: 0.25, fov: 35 },
-			],
-			onUpdate: () => {
-				this.experience.camera.instance.fov = this.experience.camera.options.fov
-				this.experience.camera.instance.updateProjectionMatrix()
-			},
-		})
-		this.options.scene = sectionNumber
+
+		if (this.paint.position.z !== this.options.sceneParams[sectionNumber].paintZPosition) {
+			gsap.to(this.camera.options, {
+				keyframes: [
+					{ ease: 'power1.in', duration: 0.75, fov: 70 },
+					{ ease: 'power1.out', duration: 0.25, fov: 35 },
+				],
+				onUpdate: () => {
+					this.camera.instance.fov = this.camera.options.fov
+					this.camera.instance.updateProjectionMatrix()
+				},
+			})
+		}
 	}
 
 	setAnimation() {
@@ -68,27 +71,30 @@ export default class SceneComponent {
 
 		this.options.sceneParams.forEach((_, index) => {
 			ScrollTrigger.create({
-				trigger: `.section${index + 1}`,
+				trigger: `.section${index}`,
 				markers: true,
 				start: 'top top',
 				onToggle: (self) => {
 					if (!self.isActive) return
 					this.setSection(index)
+					if (this.debugFolder) this.debugFolder.refresh()
 				},
 			})
 		})
 	}
 
 	setDebug() {
-		this.debug.ui
+		this.debugFolder = this.debug.ui
+
+		this.debugFolder
 			.addBinding(this.options, 'scene', {
 				label: 'scene',
-				min: 1,
-				max: this.options.sceneParams.length,
+				min: 0,
+				max: this.options.sceneParams.length - 1,
 				step: 1,
 			})
 			.on('change', ({ value }) => {
-				this.setSection(value - 1)
+				this.setSection(value)
 			})
 	}
 }
