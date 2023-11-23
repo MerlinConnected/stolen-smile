de6fd68363bdfeee509f1c8d3d1d43b2f3972d64
FlanB <clement.renou@mmibordeaux.com>
2023-11-23 23:32:59 +0100

    feat(transition-shader): test shader

--- src/WebGL/Components/SceneComponent.js
+++ src/WebGL/Components/SceneComponent.js
@@ -46,6 +46,46 @@ export default class SceneComponent {
 		this.roomModel.position.z = -16
 		this.roomModel.name = 'roomScene'
 		this.scene.add(this.roomModel)
+
+		this.roomModel.traverse((child) => {
+			if (child.material?.name.includes('Paper')) {
+				//shader reveal effect from center (use fragment shader)
+				child.material.onBeforeCompile = (shader) => {
+					shader.fragmentShader = `
+						uniform float uProgress;
+						varying vec2 vUv;
+						${shader.fragmentShader}
+					`
+					console.log(shader.fragmentShader)
+					shader.fragmentShader = shader.fragmentShader.replace(
+						`#include <dithering_fragment>`,
+						`#include <dithering_fragment>
+						float distance = distance(vUv, vec2(0.4, 0.42));
+
+						float alpha = smoothstep(uProgress, uProgress + 0.1, distance);
+						vec4 placeholderColor = vec4(vec3(0.95, 0.81, 0.62), 1.0);
+						vec4 color = mix(gl_FragColor, placeholderColor, alpha);
+						gl_FragColor = vec4(color.rgb, 1.0);
+					`,
+					)
+					this.fragmentUniforms = shader.uniforms
+					this.fragmentUniforms.uProgress = { value: 1 }
+
+					const vertexShader = shader.vertexShader
+					shader.vertexShader = `
+						varying vec2 vUv;
+						${vertexShader}
+					`
+					shader.vertexShader = shader.vertexShader.replace(
+						`#include <begin_vertex>`,
+						`
+						#include <begin_vertex>
+						vUv = uv;
+					`,
+					)
+				}
+			}
+		})
 	}
 
 	setShopModel() {
@@ -68,7 +108,6 @@ export default class SceneComponent {
 				if (!this.options.isChanging) return
 				const progress = paintTween.progress()
 				if (progress > 0.6) {
-					console.log('end')
 					this.options.isChanging = false
 				}
 			},
@@ -81,6 +120,19 @@ export default class SceneComponent {
 			ease: this.options.isChanging ? 'power2.out' : 'power2.inOut',
 			overwrite: 'auto',
 		})
+
+		if (sectionNumber === 1) {
+			this.fragmentUniforms.uProgress.value = 0
+			gsap.to(this.fragmentUniforms.uProgress, {
+				value: 0.6,
+				duration: 10,
+				ease: 'power4.out',
+				overwrite: true,
+				onUpdate: () => {
+					console.log(this.fragmentUniforms.uProgress.value)
+				},
+			})
+		}
 
 		// if (!isReverse) {
 		// 	gsap.to(this.camera.sceneCamera, {
