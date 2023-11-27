fdd6f83fd8bfdc108f177af67711f6d544a76cc3
Gaëtan Jestin <gaetanjestin@outlook.com>
2023-11-27 22:05:30 +0100

    fix(camera rig): updated rig nudge amount

--- src/WebGL/Camera.js
+++ src/WebGL/Camera.js
@@ -109,12 +109,12 @@ export default class Camera {
 	update() {
 		this.sceneCamera.position.x = MathUtils.lerp(
 			this.sceneCamera.position.x,
-			this.mouse.x * 0.4 + this.options.position.x,
+			this.mouse.x + this.options.position.x,
 			0.05,
 		)
 		this.sceneCamera.position.y = MathUtils.lerp(
 			this.sceneCamera.position.y,
-			this.mouse.y * 0.4 + this.options.position.y,
+			this.mouse.y + this.options.position.y,
 			0.05,
 		)
 		this.sceneCamera.lookAt(this.options.target)
